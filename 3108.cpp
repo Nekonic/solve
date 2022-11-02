05bca206905d71c30fa62da334eb27c28695df7f
Nekonic <97237986+Nekonic@users.noreply.github.com>
2022-11-02 15:36:56 +0900

    정올 참여 학생 리스트 만들기 1

--- 3108.cpp
+++ 3108.cpp
@@ -1,16 +1,42 @@
-#include <stdio.h>
+#include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-struct x{
-    char code;
-    int no;
+struct x {
+    int num;
     char name[11];
 };
+bool cmp1(const x& a, const x& b) {
+    if (a.num < b.num)return true;
+    return false;
+}
 vector<x> a;
-int main(){
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    x data1;
     int n;
-    scanf("%d",&n);
-    for(int i=0; i<n; i++){
-        scanf("%d",);
+    char code;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int m=-1;
+        cin >> code;
+        cin >> data1.num;
+        cin >> data1.name;
+        for(int i=0; i<a.size(); i++){
+                if(a[i].num==data1.num)m=i;
+            }
+        if (code == 'I'){
+            if(m==-1)a.push_back(data1);
+        }
+        else if (code == 'D'&&m!=-1) {
+            a.erase(a.begin()+m);
+        }
+    }
+    sort(a.begin(), a.end(), cmp1);
+    for (int i = 0; i < 5; i++) {
+        cin >> n;
+        cout << a[n-1].num << " " << a[n-1].name << "\n";
     }
 }
