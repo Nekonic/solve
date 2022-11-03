70961ae16d52bb6562c4b57a8bfd576e954dcba5
Nekonic <97237986+Nekonic@users.noreply.github.com>
2022-11-03 22:57:59 +0900

    수열

--- 4726.cpp
+++ 4726.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n, k;
+    int n, k, a[100000], s=0;
     cin >> n >> k;
-    vector<int> sum(n, 0), arr(n, 0);
-    for (int i = 0; i < n; i++){
-        cin >> arr[i];
-    }
-    for(int i=0; i<n; i++){
-        for(int j=i; j<i+k; j++){
-            sum[i]+=arr[j];
+    vector<int> sum;
+    for (int i = 1; i <= n; i++){
+        cin >> a[i];
+        s+=a[i];
+        if(i>=k){
+            sum.push_back(s);
+            s-=a[i-k+1];
         }
     }
-    int max=sum[0];
-    for(int i = 1; i < n; i++){
-        if(max<sum[i])max=sum[i];
-    }
-    cout << max;
+    sort(sum.begin(),sum.end(),greater<int>());
+    cout << sum.front();
 }
