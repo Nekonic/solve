7d6404f4ee95d19c3b6997ce61eea1665f07fd60
Nekonic <97237986+Nekonic@users.noreply.github.com>
2022-11-06 19:03:36 +0900

    K번째 소수

---
+++ BOJ15865.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+bool isPrime(int n){
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    return true;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n,vel,idx=0;
+    cin >> n;
+    for(int i=2; idx<n; i++){
+        if(isPrime(i)){
+            vel=i;
+            idx++;
+        }
+    }cout << vel;
+}
