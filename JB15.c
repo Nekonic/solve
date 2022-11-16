345a95c6d5569de602b66d11123ebdf9e1e4fe6e
Nekonic <97237986+Nekonic@users.noreply.github.com>
2022-11-16 17:42:41 +0900

    배수 369

--- JB15.c
+++ JB15.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main(){
+int main()
+{
     int n;
-    scanf("%d", &n);
+    scanf("%d",&n);
     while(n--){
-        char x[10001]={};
+        char x[1001]={};
         scanf("%s",x);
         int sum=0;
         int half=x[strlen(x)-1]-'0';
         for(int i=0; i<strlen(x); i++){
             int temp=x[i];
-            sum=((temp-48)%9+sum)%9;
-        }
-        if(sum%9==0)printf("9");
+            sum+=(temp-48);
+        }if(sum%9==0)printf("9");
         else if(half%2==0&&sum%3==0)printf("6");
         else if(sum%3==0)printf("3");
         else printf("1");
