686c55ac6c2ab0dded755d1d2d5f3a0e7b74ca27
Nekonic <97237986+Nekonic@users.noreply.github.com>
2022-11-16 17:57:32 +0900

    배수 369

--- JB15.c
+++ JB15.c
@@ -12,7 +12,7 @@ int main()
         int half=x[strlen(x)-1]-'0';
         for(int i=0; i<strlen(x); i++){
             int temp=x[i];
-            sum+=(temp-48);
+            sum=((temp-48)%9+sum)%9;
         }if(sum%9==0)printf("9");
         else if(half%2==0&&sum%3==0)printf("6");
         else if(sum%3==0)printf("3");
