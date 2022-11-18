e096e01872aef3d94847c6baa8cb24c43ecca550
Nekonic <97237986+Nekonic@users.noreply.github.com>
2022-11-18 17:20:16 +0900

    Update asciicode.c

--- asciicode.c
+++ asciicode.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 void binary(int n) {
     if(n/2)binary(n/2);
     printf("%d", n%2);
 }
 int main(){
-    char a[100]="Honam Jeil";
+    char a[100]="If will be soon";
     for(int i=0; i<strlen(a); i++){
-        //binary(a[i]);
         printf("%d ",a[i]);
     }printf("\n");
     for(int i=0; i<strlen(a); i++){
