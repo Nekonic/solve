368a64dee4236a1158fe1b43bd4f32d429daa10d
Nekonic <kybplay1234@gmail.com>
2022-11-25 16:31:14 +0900

    별 찍기 - 5

---
+++ BOJ10757.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string a,b,c;
+    cin >> a >> b;
+    int n=a.length()>b.length()?a.length():b.length(),temp;
+    for(int i=0; i<n; i++){
+        if(i==n-1&&)
+        temp+=a[i]+b[i];
+        if(temp>9){
+            c.push_back(temp);
+        }else temp=0;
+    }
+    
+}
