ecb11be7d023684c767d3ded47908ab414db155e
Nekonic <kybplay1234@gmail.com>
2022-11-30 16:15:20 +0900

    접미사 배열

---
+++ BOJ11656.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[1001],str1[1001][1001],temp[1001];
+    scanf("%s",str);
+    for(int i=0; i<strlen(str); i++){
+        for(int j=i; j<strlen(str); j++){
+            str1[i][j-i]=str[j];
+        }
+    }for(int i=0; i<strlen(str); i++){
+        for(int j=i; j<strlen(str); j++){
+            if(strcmp(str1[i],str1[j])>0){
+                strcpy(temp,str1[j]);
+                strcpy(str1[j],str1[i]);
+                strcpy(str1[i],temp);
+            }
+        }
+    }
+    for(int i=0; i<strlen(str); i++){
+        printf("%s\n",str1[i]);
+    }
+}
