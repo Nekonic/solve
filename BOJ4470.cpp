31104c3c09db74dbe53b388eafed12d2e3bcf128
Nekonic <97237986+Nekonic@users.noreply.github.com>
2022-12-26 21:06:36 +0900

    줄번호

---
+++ BOJ4470.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    cin.ignore();
+    for(int i=1; i<=n; i++){
+        string s;
+        getline(cin,s);
+        cout << i << ". " << s << '\n';
+    }
+}
